1a342924c864b520e3bfefacef06580db5bb2fc7
Fletcher0132 <fletchermoore2310@gmail.com>
2020-12-05 21:38:29 +0000

    PatientData.cs sql query added

--- ApplicationTier/PatientData.cs
+++ ApplicationTier/PatientData.cs
@@ -10,10 +10,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+
 namespace ApplicationTier
 {
-    class PatientData
+    public class PatientData
     {
+        public DataSet NHSRegNo(String connectionData)
+        {
+            //NHS Registration Number
+            string regNo = "";      //Pull this from input
+            //SQL Query
+            string selectSQL = "Select NHSRegNum, FirstName, LastName, Street Address, Town, Postcode, MedicalCon from patient where NHSRegNum = '" + regNo + "';";
 
+            //connect to database, use query and store
+            SqlConnection connect = new SqlConnection(connectionData);
+            SqlDataAdapter adapter = new SqlDataAdapter(selectSQL, connect);
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+
+            return dataset;
+        }
     }
 }
