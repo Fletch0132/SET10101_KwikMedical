752dfd4e65fa2c08865c501b0fe5d6be8ea6e06c
Fletcher0132 <fletchermoore2310@gmail.com>
2020-12-05 21:17:20 +0000

    PatientData.cs Created

--- ApplicationTier/PatientData.cs
+++ ApplicationTier/PatientData.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//Author: Fletcher Thomas Moore
+//Name: PatientData.cs
+//Description: Class to read/Query database, select specific data, and store the specific data
+//Start Date: 28/11/2020
+//Last Edit: 
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +14,6 @@ namespace ApplicationTier
 {
     class PatientData
     {
+
     }
 }
